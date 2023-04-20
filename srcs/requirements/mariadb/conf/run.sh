7e25cee1fa889546f7080a4af330d10df4f73179
Brahim Berkasse <berkassebrahim@gmail.com>
2023-04-20 02:59:47 +0000

    🍞 fix env vars in mysql

---
+++ srcs/requirements/mariadb/conf/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+cat <<EOF > db.sql
+CREATE USER '$MSQL_USER'@'%' IDENTIFIED BY '$MSQL_PASS';
+GRANT ALL PRIVILEGES ON wp.* TO '$MSQL_USER'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE wp;
+EOF 
+
+mysql < db.sql
+
+exec $@
